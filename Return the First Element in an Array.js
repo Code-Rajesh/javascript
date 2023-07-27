49bdedac711cec6412b09adb9b8c89dae0284696
Code-Rajesh <itshakti2017@gmail.com>
2023-07-27 14:27:08 +0530

    get firts elemetn of array

---
+++ Return the First Element in an Array.js
@@ -0,0 +1,7 @@
+// Return the First Element in an Array
+
+let firstarray = [1,2,3,5,'raj','rahul'];
+
+let arrayretrun = a=>firstarray[a];
+
+console.log("retrun first element of array is =",firstarray[1]);
