edb4a2510cb467a7f04d044f10b8c0200c5b0ba2
Code-Rajesh <itshakti2017@gmail.com>
2023-07-27 15:15:46 +0530

    new way array first element

--- Return the First Element in an Array.js
+++ Return the First Element in an Array.js
@@ -1,7 +1,14 @@
 // Return the First Element in an Array
 
-let firstarray = [1,2,3,5,'raj','rahul'];
+// let firstarray = [1,2,3,5,'raj','rahul'];
 
-let arrayretrun = a=>firstarray[a];
+// let arrayretrun = a=>firstarray[a];
 
-console.log("retrun first element of array is =",firstarray[1]);
+// console.log("retrun first element of array is =",firstarray[1]);
+ 
+let newarr =[1,2,3,4];
+function getFirstValue(arr) {
+	return newarr[arr];
+}
+
+console.log(getFirstValue(1));
