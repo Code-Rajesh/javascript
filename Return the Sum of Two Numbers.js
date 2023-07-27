c8f58433d54d5923c8fdecb6d3e98585be9f744c
Code-Rajesh <itshakti2017@gmail.com>
2023-07-27 13:08:58 +0530

    multlilple way of addition

--- Return the Sum of Two Numbers.js
+++ Return the Sum of Two Numbers.js
@@ -1,7 +1,20 @@
 // Return the Sum of Two Numbers
-
+// first way
 function sum(a,b){
     return a+b;
 }
 
 console.log(sum(5,9));
+
+//second way
+
+let sum = function(a,b){
+    return a+b;
+}
+
+console.log(sum(5,8));
+
+//third way
+
+let sum = (a,b)=>a+b;
+console.log('sum of two number is =',sum(78,85));
