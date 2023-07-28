0540aa24affa8f60ae2349fb5810690905274789
Code-Rajesh <itshakti2017@gmail.com>
2023-07-28 21:53:32 +0530

    string pass as parameter

---
+++ ReturnfomethingtoMe!.js
@@ -0,0 +1,10 @@
+// ReturnfomethingtoMe!
+
+// let returnstr = a=>a;
+
+// console.log(returnstr("rajeshkumar"));
+function giveMeSomething(a) {
+	return "something " + a;
+}
+
+console.log(giveMeSomething("rajesh"));
