1366bba725e2b7a1a826b0e4972b6647cbf5af73
Code-Rajesh <itshakti2017@gmail.com>
2023-07-31 20:29:37 +0530

    today date display

---
+++ display the current day and time .js
@@ -0,0 +1,6 @@
+// Write a JavaScript program to display the current day and time in the following format.  
+// Sample Output : Today is : Tuesday.
+// // Current time is : 10 PM : 30 : 38
+
+const todaydate = new Date();
+document.getElementById("date").innerHTML = todaydate;
